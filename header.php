b3b7b7d00dbd86fa99db5b7bb8de154a2c2c6830
Katelya99 <121582340+Katelya99@users.noreply.github.com>
2022-12-28 23:55:47 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>صفحة بيكو الرياضية  </title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+
+</head>
+<body>
+	<header>
+	<center>
+<h1 > صفحة بيكو الرياضية  </h1>
+<h2 class="dany"> اخر الاخبار </H2>
+<h2 class="dany"> مباريات اليوم </h2>
+<h2 class="dany"> مباريات الامس </H2>
+<h2 class="dany" style="background: red; "> مباريات الغد </H2>
+	</header>
+	
+</center>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> صفحة الدخول </title>
+	<style type="text/css">
+		div{
+			top: 200px;
+			position: relative;
+			background: blue;
+			width: 33%;
+			left: 33%;
+
+		}
+		input{
+			font-size: 28px;
+			position: relative;
+			width: 66%;
+		}
+		label{
+			font-size: 28px;
+			position: relative;
+			width: 34%;
+		}
+	</style>
+</head>
+<body>
+	<div>
+	<center>
+<form>
+	<input type="" name="" placeholder=" اكتب اسم المستخدم ">
+	<table> اسم المستخدم </table>
+	<input type="password" name="" placeholder=" اكتب كلمةالسر ">
+	<table> كلمة السر </table>
+	<input style="width: 100%" type="submit" name="">
+</form>
+<center>
+<a href=""> ليس لدي حساب </a>
+<a href="index.html"><button> رجوع </button></a>
+</center>
+</div>
+</body>
+</html>
